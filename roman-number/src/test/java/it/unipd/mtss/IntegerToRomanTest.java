0585d472f938d78a3dfc2dde9662a6da75b09a59
francescomarcon6 <francesco.marcon.6@studenti.unipd.it>
2026-05-09 00:45:54 +0200

    aggiunti file java per i test

---
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////
+// FRANCESCO MARCON 2110990
+// MARCO SANGUIN 2103121
+////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class IntegerToRomanTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, I" // TODO: Aggiungere gli altri casi di test qui
+    })
+    public void convert_ValidInteger_ReturnsRomanString(int input, String expectedOutput) {
+        // TODO: Arrange
+        
+        // TODO: Act
+        
+        // TODO: Assert
+        
+    }
+}
