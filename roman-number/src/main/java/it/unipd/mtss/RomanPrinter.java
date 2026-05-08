1044738685efdd7897b990e8119481b84e68033d
francescomarcon6 <francesco.marcon.6@studenti.unipd.it>
2026-05-09 01:22:10 +0200

    aggiornato RomanPrinter

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -6,30 +6,30 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
-    
-    public static String print(int num) {
-        return printAsciiArt(IntegerToRoman.convert(num));
+
+  public static String print(int num) {
+    return printAsciiArt(IntegerToRoman.convert(num));
+  }
+
+  private static String printAsciiArt(String romanNumber) {
+    StringBuilder asciiArt = new StringBuilder();
+    String[][] asciiLetters = {
+      {" _____ ", "|_   _|", "  | |  ", "  | |  ", " _| |_ ", "|_____|"},
+      {"__      __", "\\ \\    / /", " \\ \\  / / ",
+      "  \\ \\/ /  ", "   \\  /   ", "    \\/    "},
+    };
+    String letters = "IV";
+
+    for (int row = 0; row < 6; row++) {
+      for (char c : romanNumber.toCharArray()) {
+        int index = letters.indexOf(c);
+        asciiArt.append(asciiLetters[index][row]);
+      }
+      if (row < 5) {
+        asciiArt.append("\n");
+      }
     }
-    
-    private static String printAsciiArt(String romanNumber) {
-        StringBuilder asciiArt = new StringBuilder();
-        String[][] asciiLetters = {
-                { " _____ ", "|_   _|", "  | |  ", "  | |  ", " _| |_ ", "|_____|" },
-                { "__      __", "\\ \\    / /", " \\ \\  / / ", "  \\ \\/ /  ", "   \\  /   ", "  
-                  \\/    " },
-        };
-        String letters = "IV";
-        
-        for (int row = 0; row < 6; row++) {
-            for (char c : romanNumber.toCharArray()) {
-                int index = letters.indexOf(c);
-                asciiArt.append(asciiLetters[index][row]);
-            }
-            if (row < 5) {
-                asciiArt.append("\n");
-            }
-        }
-        
-        return asciiArt.toString();
-    }
+
+    return asciiArt.toString();
+  }
 }
