0785cd18bbaf0ac7afe852b630af80bec2759913
francescomarcon6 <francesco.marcon.6@studenti.unipd.it>
2026-05-09 01:04:46 +0200

    aggiunto codice per numeri fino a 3

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -4,9 +4,21 @@
 ////////////////////////////////////////////
 
 package it.unipd.mtss;
+
 public class IntegerToRoman {
-    public static String convert(int number){
-        // TODO
-        return null;
+
+    public static String convert(int number) {
+        String[] romanLetters = { "I" };
+        int[] values = { 1 };
+        StringBuilder romanNumber = new StringBuilder();
+        
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                number -= values[i];
+                romanNumber.append(romanLetters[i]);
+            }
+        }
+        
+        return romanNumber.toString();
     }
 }
--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -6,12 +6,28 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
-    public static String print(int num){
+    
+    public static String print(int num) {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
     
-    private static String printAsciiArt(String romanNumber){
-        //TODO
-        return null;
+    private static String printAsciiArt(String romanNumber) {
+        StringBuilder asciiArt = new StringBuilder();
+        String[][] asciiLetters = {
+                { " _____ ", "|_   _|", "  | |  ", "  | |  ", " _| |_ ", "|_____|" }
+        };
+        String letters = "I";
+        
+        for (int row = 0; row < 6; row++) {
+            for (char c : romanNumber.toCharArray()) {
+                int index = letters.indexOf(c);
+                asciiArt.append(asciiLetters[index][row]);
+            }
+            if (row < 5) {
+                asciiArt.append("\n");
+            }
+        }
+        
+        return asciiArt.toString();
     }
 }
